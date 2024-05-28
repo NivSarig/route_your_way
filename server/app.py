ba576fcf659553eda2a5fe1a2847a2e6ab73399d
Nir Radian <nir.radian@optibus.com>
2024-05-28 17:19:31 +0300

    added requirements

--- server/app.py
+++ server/app.py
@@ -1,7 +1,11 @@
 from fastapi import BackgroundTasks, FastAPI, HTTPException
 
+import sys, os
 from game import add_contestant, add_submit, create_game, get_game
 
+sys.path.append(os.getcwd() + "/..")
+
+from external_integrations.gmaps_integration_utils import generate_random_coordinates
 
 app = FastAPI()
 
@@ -38,8 +42,9 @@ async def put_done(game_id: str = None):
 async def get_game_endpoint(game_id: str = None):
     return get_game(game_id)
 
+
 @app.put("/game/{game_id}/submit/{url}")
-async def submit(game_id: str, name: str, url:str , background_tasks: BackgroundTasks):
+async def submit(game_id: str, name: str, url: str, background_tasks: BackgroundTasks):
     curr_game = get_game(game_id)
     if not name:
         # create an exception
