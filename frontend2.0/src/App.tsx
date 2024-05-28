6a2ad935ca5142e62b44508f52e4fbce6f7bce27
galgendler <gal.gendler@optibus.com>
2024-05-28 14:18:17 +0300

    initial

---
+++ frontend2.0/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MapWithPolyline from './MapWithPolyline';
+
+function App() {
+  return (
+    <div className="App">
+      <MapWithPolyline/>
+
+    </div>
+  );
+}
+
+export default App;
