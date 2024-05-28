0af75ccc4963a9d13d8e16a6745d22ed1f1bf208
Gony Shapira <gony.shapira@optibus.com>
2024-05-28 17:52:08 +0300

    use material

--- frontend4.0/src/LandingPage.tsx
+++ frontend4.0/src/LandingPage.tsx
@@ -1,15 +1,31 @@
-import styled from "styled-components";
+import { styled } from "@mui/material";
 import img from "./background.png";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
-const Container = styled("div")`
-  background-image: url(${img});
-  background-size: 100% 100%;
-  height: 100vh;
-  width: 100vw;
-`;
+const Container = styled("div")({
+  backgroundImage: `url(${img})`,
+  backgroundSize: "100% 100%",
+  height: "100vh",
+  width: "100vw",
+});
 
 function LandingPage() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={10}
+        label="Age"
+        // onChange={handleChange}
+      >
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+    </Container>
+  );
 }
 
 export default LandingPage;
