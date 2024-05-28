53e84c5f5bc4d2aff94f0d76098a897dea0378be
litalKakun <lital.kakun@optibus.com>
2024-05-28 16:36:20 +0300

    add background process

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import BackgroundTasks, FastAPI, HTTPException
 
 from game import add_contestant, add_submit, create_game, get_game
 
@@ -39,12 +39,16 @@ async def get_game_endpoint(game_id: str = None):
     return get_game(game_id)
 
 @app.put("/game/{game_id}/submit/{url}")
-async def submit(game_id: str = None, name: str = None, url: str = None):
-    get_game(game_id)
+async def submit(game_id: str, name: str, url:str , background_tasks: BackgroundTasks):
+    curr_game = get_game(game_id)
     if not name:
         # create an exception
         raise HTTPException(status_code=400, detail="Name cannot be empty")
     if not url:
         raise HTTPException(status_code=400, detail="URL cannot be empty")
-    add_submit(game_id, name, url)
-    return get_game(game_id)
+    curr_contestant = curr_game["contestants"][name]
+    if "status" in curr_contestant and curr_contestant["status"] == "processing":
+        raise HTTPException(status_code=400, detail="Contestant already submitted")
+    curr_contestant["status"] = "processing"
+    background_tasks.add_task(add_submit, game_id, name, url)
+    return curr_game
