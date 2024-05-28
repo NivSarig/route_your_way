93b4945f48fa06b4be8b87f9099d55a169a5bed5
Gony Shapira <gony.shapira@optibus.com>
2024-05-28 15:18:52 +0300

    add frontend4.0

---
+++ frontend4.0/src/Marker.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Circle as GoogleMapsMarker } from "@react-google-maps/api";
+
+const Marker = ({ position, text, onClick, onMouseOver }) => {
+  return (
+    <GoogleMapsMarker
+      // onClick={onClick}
+      // onMouseOver={onMouseOver}
+      center={position}
+      options={{
+        fillColor: "red",
+        fillOpacity: 1,
+        radius: 600,
+        // clickable: true,
+        // draggable: false,
+      }}
+      // zIndex={999}
+      // icon={{
+      //   path: google.maps.SymbolPath.CIRCLE,
+      //   scale: 6,
+      //   strokeColor: "red",
+      //   fillColor: "red",
+      //   fillOpacity: 1,
+      // }}
+      // label={{
+      //   text: text || "",
+      //   color: "black",
+      //   fontSize: "40px",
+      // }}
+    />
+  );
+};
+
+export default Marker;
