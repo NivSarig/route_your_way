d121a093fe39940fb66d55eb2d872cbda1447351
niv.sarig <niv@optibus.com>
2024-05-28 16:17:04 +0300

    wip

--- main_script.py
+++ main_script.py
@@ -1,8 +1,9 @@
-from route_your_way import generate_random_coordinates, get_coordinates_from_url, get_distance_and_duration, log, \
-    build_all_duration_matrix, coordinate_to_str, formatted_now
+from external_integrations.gmaps_integration_utils import generate_random_coordinates, build_all_duration_matrix, \
+    coordinate_to_str, \
+    get_route_info_from_url, get_url_from_origin_waypoints_and_destination
 import requests
 
-from tsp_model_solver import solve_tsp_model
+from logging_utils import log, formatted_now
 
 if __name__ == "__main__":
     randomize = False
@@ -27,8 +28,7 @@ if __name__ == "__main__":
     log(coordinates)
 
     new_url = "https://www.google.com/maps/dir/55.78879096471749+37.56912052075894/55.82394492336372+37.71564443590594/55.718563249961306+37.51647539470249/55.775807492867564+37.51946053900027/55.690901822366115+37.46013158008646/55.80069715769361+37.56576992130943/55.60486832825113+37.466155203107114/55.78130697817363+37.46718729666109/55.70319444748+37.63345154478967/55.843094719260534+37.47741722330984/@55.7272268,37.3982661,11z/data=!3m1!4b1!4m62!4m61!1m5!1m1!1s0x0:0xfebb1ddabbd58077!2m2!1d37.5685818!2d55.7885064!1m5!1m1!1s0x0:0x865d4de12b4564b6!2m2!1d37.716047!2d55.8236744!1m5!1m1!1s0x0:0x5df7cd41dca8f173!2m2!1d37.5139163!2d55.7061335!1m5!1m1!1s0x0:0x8439f8542df1d9a4!2m2!1d37.5198347!2d55.7759439!1m5!1m1!1s0x0:0xd0422de2cbc7203f!2m2!1d37.4600474!2d55.6907233!1m5!1m1!1s0x0:0x9e8663cc32912aa7!2m2!1d37.5659516!2d55.8008145!1m5!1m1!1s0x0:0x23dd95533621b43b!2m2!1d37.4623676!2d55.6028596!1m5!1m1!1s0x0:0x76d30e134b59d2a1!2m2!1d37.4773823!2d55.7750483!1m5!1m1!1s0x0:0xfd468a8f9cf66ed8!2m2!1d37.6333575!2d55.7028242!1m5!1m1!1s0x0:0xa0c2634c7f03102d!2m2!1d37.4773427!2d55.8429622!3e0?entry=ttu"
-    new_coordinates = get_coordinates_from_url(new_url)
-    distance, duration = get_distance_and_duration(new_coordinates)
+    distance, duration = get_route_info_from_url(new_url)
     log((distance, duration))
 
     short_coordinates = [
@@ -46,8 +46,8 @@ if __name__ == "__main__":
         (55.82000118014139, 37.531695602671846)]
     coordinate_to_str(origin)
     coordinate_to_str(destination)
-    "https://maps.googleapis.com/maps/api/directions/json?origin=55.70319444748,37.63345154478967&destination=55.843094719260534,37.47741722330984&waypoints=55.76256915886491,37.53964491245408|55.79953159156407,37.62388899803995|55.82000118014139,37.531695602671844&key=AIzaSyDP0EV22kIb6LHSh3zEABMe1CTxwzwSdWs"
-    url_with_waypoints = f"https://maps.googleapis.com/maps/api/directions/json?origin={coordinate_to_str(origin)}&destination={coordinate_to_str(destination)}&waypoints={concatenate_coordinates(coordinates_with_waypoints)}&key=AIzaSyDP0EV22kIb6LHSh3zEABMe1CTxwzwSdWs"
+    coordinates_with_waypoints = "https://maps.googleapis.com/maps/api/directions/json?origin=55.70319444748,37.63345154478967&destination=55.843094719260534,37.47741722330984&waypoints=55.76256915886491,37.53964491245408|55.79953159156407,37.62388899803995|55.82000118014139,37.531695602671844&key=AIzaSyDP0EV22kIb6LHSh3zEABMe1CTxwzwSdWs"
+    url_with_waypoints = get_url_from_origin_waypoints_and_destination(origin, destination, coordinates_with_waypoints)
     print(url)
     response = requests.get(url_with_waypoints, timeout=1)
     data = response.json()
