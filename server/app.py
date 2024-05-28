064574a49182d7aefc7d71dd11a01357e5eac5ae
Nir Radian <nir.radian@optibus.com>
2024-05-28 14:39:52 +0300

    join and create work

--- server/app.py
+++ server/app.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from game import add_contestant, create_game, get_game
+
 
 app = FastAPI()
 
@@ -6,3 +9,18 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.put("/game")
+async def put_game(location: str = None):
+    return create_game(location)
+
+
+@app.put("/game/{game_id}/contestant")
+async def put_contestant(game_id: str = None, name: str = None):
+    get_game(game_id)
+    if not name:
+        # create an exception
+        raise HTTPException(status_code=400, detail="Name cannot be empty")
+    add_contestant(game_id, name)
+    return get_game(game_id)
---
+++ server/game.py
@@ -0,0 +1,40 @@
+game_dict = {}
+from utils import generate_random_string
+from fastapi import HTTPException
+
+
+def get_game(game_id):
+    # throw exception if game_id doesn't exist
+    if game_id not in game_dict:
+        raise HTTPException(status_code=404, detail="Game not found")
+    return game_dict[game_id]
+
+
+def create_game(location, code=None):
+
+    new_game_id = code or generate_random_string()
+    print("creating game", new_game_id)
+
+    game_dict[new_game_id] = {
+        "location": location,
+        "game_id": new_game_id,
+        "contestants": [],
+    }
+
+    return get_game(new_game_id)
+
+
+def check_existing_name(game_id, name):
+    for contestant in game_dict[game_id]["contestants"]:
+        if contestant["name"] == name:
+            raise HTTPException(status_code=400, detail="Contestant already exists")
+
+
+def add_contestant(game_id, name):
+    check_existing_name(game_id, name)
+
+    game_dict[game_id]["contestants"].append({"name": name})
+    return get_game(game_id)
+
+
+create_game("TEST", "TEST")
