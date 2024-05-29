29e8122cd2292caa6a288a3ca8eae25e346118b5
Nir Radian <nir.radian@optibus.com>
2024-05-29 16:44:39 +0300

    add more cities, different random

--- frontend4.0/src/CreateGamePage.tsx
+++ frontend4.0/src/CreateGamePage.tsx
@@ -42,7 +42,7 @@ const StyledButton = styled(Button)(({ disabled }) => ({
   width: disabled ? "100px" : "140px",
   marginTop: disabled ? "70px" : "50px",
 }));
-const cities = ["London", "Tel Aviv"];
+const cities = ["London", "Tel Aviv", "Paris", "New York", "Berlin"];
 
 function CreateGamePage() {
   const [city, setCity] = useState<string>("");
