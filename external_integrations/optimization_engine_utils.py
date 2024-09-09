c873f8369210f8a1e34ed67ec305f14133e0e417
niv.sarig <niv@optibus.com>
2024-09-09 23:04:23 +0300

    Fix model symmetry and circularity bugs

--- external_integrations/optimization_engine_utils.py
+++ external_integrations/optimization_engine_utils.py
@@ -1,5 +1,6 @@
 import os
 import time
+from copy import deepcopy
 from random import random
 
 from external_integrations.gmaps_integration_utils import (
@@ -66,7 +67,7 @@ def solve_tsp_from_coordinate_list(coordinates_list, game_id, use_cache):
     deadhead_index = build_all_duration_matrix(coordinates_list, use_cache)
 
     return deadhead_index, solve_tsp_for_deadhead_index(
-        deadhead_index, game_directory, game_id, mock=MOCK
+        deepcopy(deadhead_index), game_directory, game_id, mock=MOCK
     )
 
 
@@ -74,39 +75,90 @@ def solve_tsp_for_deadhead_index(deadhead_index, game_dir, game_id, mock=MOCK):
     if not os.path.exists(game_dir):
         os.mkdir(game_dir)
     input_file_name = os.path.join(game_dir, "input.tsp")
-    output_file_name = os.path.join(game_dir, "output.txt")
-    # output_file_name = os.path.join(game_dir, "asym-good_output.txt")
+    output_file_name = os.path.join(game_dir, "output.res")
+
     if os.path.exists(output_file_name) and mock is not False:
         time.sleep(1.5 + random())
         with open(output_file_name, "r") as fid:
             return fid.read().split("\n")
 
-    if mock == "brute":
-        all_stop_indices = list(deadhead_index.keys())
-        import itertools
+    maximal_deadhead = \
+        max(set([v['duration'] for k, inner_dict in deadhead_index.items() for ik, v in inner_dict.items()]))
 
-        # Generate all permutations
-        permutations = itertools.permutations(all_stop_indices)
-        best_duration = float("inf")
-        best_perm = None
-        for permutation in permutations:
-            perm_duration = 0
-            for perm_pair in pairwise(permutation):
-                perm_duration += deadhead_index[str(perm_pair[0])][str(perm_pair[1])][
-                    "duration"
-                ]
-            if perm_duration < best_duration:
-                best_duration = perm_duration
-                best_perm = permutation
-        indices = list(best_perm)
-        with open(output_file_name, "w") as fid:
-            fid.write("\n".join(indices))
-        return indices
+    # TSP solves a circular solution (returns to the origin) We will add 2 more auxiliary nodes that will play the
+    # role of the first/last node
+
+    maxINF = maximal_deadhead  # 'small INF'
+    minMaxINF = 0*maximal_deadhead  # '0'
+
+    starting_index = str(len(deadhead_index))
+    ending_index = str(len(deadhead_index) + 1)
+    deadhead_index[starting_index] = deepcopy(deadhead_index[str(0)])
+    deadhead_index[ending_index] = deepcopy(deadhead_index[str(0)])
+    for source_node in deadhead_index:
+        # Add starting and ending index nodes to each dictionary
+
+        deadhead_index[source_node][starting_index] = deepcopy(deadhead_index[str(0)][str(0)])
+        deadhead_index[source_node][ending_index] = deepcopy(deadhead_index[str(0)][str(0)])
+
+    for node in deadhead_index:
+        deadhead_index[node][node]['origin_idx'] = node
+        deadhead_index[node][node]['destination_idx'] = node
+        deadhead_index[node][node]['distance'] = 0
+        deadhead_index[node][node]['duration'] = 0
+
+        deadhead_index[starting_index][node]['origin_idx'] = starting_index
+        deadhead_index[starting_index][node]['destination_idx'] = node
+        deadhead_index[starting_index][node]['distance'] = minMaxINF
+        deadhead_index[starting_index][node]['duration'] = minMaxINF
+        deadhead_index[starting_index][node]['origin'] = [None, None]
+        deadhead_index[starting_index][node]['destination'] = [None, None]
+
+        deadhead_index[ending_index][node]['origin_idx'] = ending_index
+        deadhead_index[ending_index][node]['destination_idx'] = node
+        deadhead_index[ending_index][node]['distance'] = maxINF
+        deadhead_index[ending_index][node]['duration'] = maxINF
+        deadhead_index[ending_index][node]['origin'] = [None, None]
+        deadhead_index[ending_index][node]['destination'] = [None, None]
+
+        deadhead_index[node][starting_index]['origin_idx'] = node
+        deadhead_index[node][starting_index]['destination_idx'] = starting_index
+        deadhead_index[node][starting_index]['distance'] = maxINF
+        deadhead_index[node][starting_index]['duration'] = maxINF
+        deadhead_index[node][starting_index]['origin'] = [None, None]
+        deadhead_index[node][starting_index]['destination'] = [None, None]
+
+        deadhead_index[node][ending_index]['origin_idx'] = node
+        deadhead_index[node][ending_index]['destination_idx'] = ending_index
+        deadhead_index[node][ending_index]['distance'] = minMaxINF
+        deadhead_index[node][ending_index]['duration'] = minMaxINF
+        deadhead_index[node][ending_index]['origin'] = [None, None]
+        deadhead_index[node][ending_index]['destination'] = [None, None]
+
+        deadhead_index[starting_index][ending_index]['origin_idx'] = starting_index
+        deadhead_index[starting_index][ending_index]['destination_idx'] = ending_index
+        deadhead_index[starting_index][ending_index]['distance'] = maxINF
+        deadhead_index[starting_index][ending_index]['duration'] = maxINF
+        deadhead_index[starting_index][ending_index]['origin'] = [None, None]
+        deadhead_index[starting_index][ending_index]['destination'] = [None, None]
+
+        deadhead_index[ending_index][starting_index]['origin_idx'] = ending_index
+        deadhead_index[ending_index][starting_index]['destination_idx'] = starting_index
+        deadhead_index[ending_index][starting_index]['distance'] = minMaxINF
+        deadhead_index[ending_index][starting_index]['duration'] = minMaxINF
+        deadhead_index[ending_index][starting_index]['origin'] = [None, None]
+        deadhead_index[ending_index][starting_index]['destination'] = [None, None]
+
     # Make a symmetric TSP out of the asymmetric problem we have
+
+    sum_of_all_deadheads = \
+        sum(set([v['duration'] for k, inner_dict in deadhead_index.items() for ik, v in inner_dict.items()]))
+
+    INF = str(sum_of_all_deadheads)  # 'large INF'
+    mINF = str(-sum_of_all_deadheads)  # '-large INF'
+
     dimension = len(deadhead_index)
     tsp_dimension = dimension * 2
-    INF = str(1000000)  # 'INF'
-    mINF = str(-1000000)  # '-INF'
     durations = [[INF] * tsp_dimension for __ in range(tsp_dimension)]
     for k1 in range(dimension):
         for k2 in range(dimension):
@@ -114,16 +166,16 @@ def solve_tsp_for_deadhead_index(deadhead_index, game_dir, game_id, mock=MOCK):
                 continue
             int_duration12 = str(deadhead_index[str(k1)][str(k2)]['duration'])
             int_duration21 = str(deadhead_index[str(k2)][str(k1)]['duration'])
-            durations[int(k1)][int(k2)] = INF
-            durations[int(k2)][int(k1)] = INF
-            durations[dimension + int(k1)][dimension + int(k2)] = INF
-            durations[dimension + int(k2)][dimension + int(k1)] = INF
-            durations[int(k1)][dimension + int(k2)] = int_duration12
-            durations[dimension + int(k2)][int(k1)] = int_duration21
-        durations[int(k1)][int(k1)] = '0'
-        durations[dimension + int(k1)][dimension + int(k1)] = '0'
-        durations[int(k1)][dimension + int(k1)] = mINF
-        durations[dimension + int(k1)][int(k1)] = mINF
+            durations[int(k1)][int(k2)] = durations[dimension + int(k1)][dimension + int(k2)] = INF
+            durations[int(k2)][int(k1)] = durations[dimension + int(k2)][dimension + int(k1)] = INF
+
+            durations[dimension + int(k1)][int(k2)] = durations[int(k2)][dimension + int(k1)] = int_duration12
+            durations[dimension + int(k2)][int(k1)] = durations[int(k1)][dimension + int(k2)] = int_duration21
+            # durations[int(k1)][dimension + int(k2)] = int_duration12
+            # durations[dimension + int(k2)][int(k1)] = int_duration21
+
+        durations[int(k1)][int(k1)] = durations[dimension + int(k1)][dimension + int(k1)] = '0'
+        durations[int(k1)][dimension + int(k1)] = durations[dimension + int(k1)][int(k1)] = mINF
 
     duration_str = []
     for line_idx, duration_line in enumerate(durations):
@@ -151,16 +203,46 @@ EOF""".format(
                 input_file_name, output_file_name
             )
         )
-        print("Woohoo we are Hackathoning")
+        print("TSP found a circular solution")
     except Exception as e:
-        print("Oh boy we are Hackathoning")
-    symmetric_output_file_name = os.path.join(game_dir, "good_output.txt")
-    symmetric_output_file_name = os.path.join(game_dir, "output.txt")
+        print("Oh boy TSP failed with error: e: {}".format(e))
+    print_full_matrix = True
+    if print_full_matrix:
+        duration_str = []
+        for line_idx, duration_line in enumerate(durations):
+            line_str = str(line_idx) + ": "
+            for col_idx, duration_item in enumerate(duration_line):
+                line_str += f"({str(line_idx)},{col_idx}):{duration_item}, "
+
+            duration_str.append(line_str)
+        duration_str = "\n".join(duration_str)
+        print(duration_str)
+    symmetric_output_file_name = os.path.join(game_dir, "output.res")
+
     with open(symmetric_output_file_name, 'r') as fid:
-        asymmetric_output_columns = fid.read().split('\n')[:-1][::2]
-    asymmetric_output_file_name = os.path.join(game_dir, "asym-good_output.txt")
+        # Remove \n and remove any auxiliary index
+        asymmetric_output_columns = fid.read().split('\n')
+        print(asymmetric_output_columns)
+        asymmetric_output_columns = asymmetric_output_columns[:-1][::2]
+        print(asymmetric_output_columns)
+
+        max1_index, max2_index = [i for i,j in sorted(list(enumerate(asymmetric_output_columns)),
+                                                      key=lambda x:int(x[1]), reverse=True)[:2]]
+
+        if max2_index < max1_index:
+            asymmetric_output_columns = list(reversed(list(asymmetric_output_columns[max1_index+1:] +
+                                                      asymmetric_output_columns[:max2_index])))
+        else:
+            asymmetric_output_columns = list(asymmetric_output_columns[max2_index+1:] +
+                                             asymmetric_output_columns[:max1_index])
+        print(asymmetric_output_columns)
+        # asymmetric_output_columns = asymmetric_output_columns[2:]
+        print(asymmetric_output_columns)
+
+    asymmetric_output_file_name = os.path.join(game_dir, "asym-good_output.res")
     with open(asymmetric_output_file_name, "w") as fid:
         fid.write("\n".join(asymmetric_output_columns))
+    print(asymmetric_output_file_name)
     return asymmetric_output_columns
 
 
