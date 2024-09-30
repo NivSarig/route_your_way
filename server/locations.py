d1993412f5f5102b4130383ce8a864c659d6c824
niv <niv@optibus.com>
2024-09-30 23:04:32 +0300

    Change London and New York coordinates and San Francisco

--- server/locations.py
+++ server/locations.py
@@ -11,18 +11,6 @@ locations = {
         [32.0711, 34.8234],
         [32.0848, 34.8113],
     ],
-    "London": [
-        [51.5074, -0.1278],
-        [51.5145, -0.0937],
-        [51.5200, -0.1557],
-        [51.4923, -0.1907],
-        [51.5363, -0.1405],
-        [51.4775, -0.0014],
-        [51.4456, -0.2065],
-        [51.5292, -0.1981],
-        [51.5527, -0.1807],
-        [51.4665, -0.1685],
-    ],
     "Paris": [
         [48.8566, 2.3522],
         [48.877807, 2.351152],
@@ -34,18 +22,6 @@ locations = {
         [48.861, 2.3995],
         [48.8683, 2.3425],
         [48.832, 2.3551],
-    ],
-    "New York": [
-        [40.7293, -73.9844],
-        [40.7380, -74.0027],
-        [40.7165, -73.9946],
-        [40.7265, -73.9515],
-        [40.7143, -73.9980],
-        [40.7041, -73.9580],
-        [40.7561, -73.9903],
-        [40.7434, -73.9505],
-        [40.6947, -73.9213],
-        [40.7484, -73.9879],
     ],
     "Sao Paulo": [
         [-23.5505, -46.6333],
@@ -59,4 +35,50 @@ locations = {
         [-23.6072, -46.6342],
         [-23.5852, -46.6742],
     ],
+    'London': [
+        [51.5194, -0.1270],  # The British Museum
+        [51.5081, -0.0759],  # Tower of London
+        [51.5115, -0.1234],  # Covent Garden
+        [51.5101, -0.1340],  # Piccadilly Circus
+        [51.4769, -0.0005],  # Greenwich Park
+        [51.5566, -0.1781],  # Hampstead Heath
+        [51.4784, -0.1542],  # Battersea Park
+        [51.5306, -0.1230],  # King's Cross Station
+        [51.4780, -0.2950],  # Kew Gardens
+        [51.4343, -0.2145],  # Wimbledon Tennis Club
+        [51.5942, -0.1300],  # Alexandra Palace
+        [51.5053, 0.0542],  # London City Airport
+        [51.4182, -0.0719],  # Crystal Palace Park
+        [51.5609, -0.0078]  # Leyton Orient Football Club
+    ],
+    'New York': [
+        [40.7580, -73.9855],  # Times Square
+        # [40.6413, -73.7781],  # JFK Airport
+        [40.5749, -73.9859],  # Coney Island
+        [40.7516, -73.8267],  # Queens Botanical Garden
+        [40.8075, -73.9626],  # Columbia University
+        [40.6718, -73.9637],  # Brooklyn Museum
+        [40.8689, -73.8100],  # Pelham Bay Park
+        # [40.7769, -73.8740],  # LaGuardia Airport
+        [40.8623, -73.8800],  # New York Botanical Garden
+        [40.5795, -73.8373],  # Rockaway Beach
+        [40.6437, -74.0736],  # Staten Island Ferry Terminal
+        [40.8104, -73.9153]  # St Mary’s Park
+    ],
+    'San Francisco': [
+        [37.7734, -122.4680],  # Golden Gate Park
+        [37.7617, -122.4468],  # Tank Hill
+        [37.7492, -122.4569],  # Laguna Hospital
+        [37.7210, -122.4441],  # Balboa Park
+        [37.7356, -122.5057],  # San Francisco Zoo
+        [37.7335, -122.4343],  # Glen Park
+        [37.7439, -122.4133],  # Bernal Heights Park
+        [37.7595, -122.5102],  # Ocean Beach
+        [37.7617, -122.4258],  # Dolores Street and 18th Street Intersection
+        [37.7404, -122.4658],  # West Portal
+        [37.7850, -122.4292],  # Japantown
+        [37.7510, -122.4332],  # Noe Valley
+        [37.7599, -122.3977],  # Potrero Hill
+        [37.7484, -122.4803],  # Sunset District
+    ]
 }
